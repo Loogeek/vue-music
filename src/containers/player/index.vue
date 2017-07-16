049decbf162d09ba72a079c7411449574635c035
Loogeek <xiaolong3956@gmail.com>
2017-07-16 23:47:37 +0800

    dev larger music play

--- src/containers/player/index.vue
+++ src/containers/player/index.vue
@@ -1,40 +1,119 @@
 <template>
-    <section class="music-player">
-        <article class="music-player-larger">
-            <header class="larger-title">
+    <section class="music-player" v-if="playSong.playList.length > 0">
+        <article class="larger" v-show="playSong.fullScreen">
+            <header class="larger-header">
                 <span class="back">
                     <i class="icon-back"></i>
                 </span>
-                <h2 class="title"></h2>
-                <h3 class="subtitle"></h3>
+                <h2 class="title" v-html="playSong.currentSong.songname"></h2>
+                <h3 class="subtitle" v-html="playSong.currentSong.singer"></h3>
             </header>
             <div class="larger-body">
                 <div class="larger-body-left">
-
+                    <img :src="playSong.currentSong.image">
                 </div>
                 <div class="larger-body-right"></div>
             </div>
-            <div class="bg-image">
-                <img src="" alt="">
+            <div class="larger-bgimg">
+                 <img :src="playSong.currentSong.image" alt=""> 
             </div>
         </article>
-        <article class="music-player-mini"></article>
+        <article class="mini" v-show="!playSong.fullScreen"></article>
     </section>
 </template>
 
 <script>
-    import { mapGetters } from 'vuex';
+import { mapGetters } from 'vuex';
 
-    export default {
-        computed: {
-            ...mapGetters([
-                'playSong'
-            ])
-        }
-    };
+export default {
+    computed: {
+        ...mapGetters([
+            'playSong'
+        ])
+    }
+};
 </script>
 
 <style lang="scss" scoped>
+@import "~common/scss/variable";
+@import "~common/scss/mixin";
 
+.music-player {
+    .larger {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 200;
+        background-color: $color-background;
+
+        &-header {
+            position: relative;
+            margin-bottom: 2.5rem;
+
+            .back {
+                position: absolute;
+                top: 0;
+                left: .6rem;
+                z-index: 50;
+
+                .icon-back {
+                    display: block;
+                    padding: 1rem;
+                    font-size: $font-size-large-x;
+                    color: $color-theme;
+                    transform: rotate(-90deg);
+                }
+            }
+
+            .title {
+                width: 70%;
+                margin: 0 auto;
+                @extend %no-wrap;
+                z-index: 50;
+                text-align: center;
+                line-height: 4rem;
+                font-size: $font-size-large;
+                color: $color-text;
+            }
+
+            .subtitle {
+                line-height: 2rem;
+                text-align: center;
+                font-size: $font-size-medium;
+                color: $color-text;
+            }
+        }
+
+        &-body {
+
+            &-left {
+                width: 80%;
+                margin: 0 auto;
+
+                > img {
+                    border-radius: 50%;
+                }
+            }
+        }
+
+        &-bgimg {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: -1;
+            opacity: .6;
+            filter: blur(2rem);
+
+            > img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+}
 </style>
 
--- src/containers/singerDetail/index.vue
+++ src/containers/singerDetail/index.vue
@@ -27,220 +27,220 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
-import Scroll from 'components/Scroll';
-import SongList from 'components/SongList';
-import Loading from 'components/Loading';
-import { prefixStyle } from 'common/js/dom';
+    import { mapActions, mapGetters } from 'vuex';
+    import Scroll from 'components/Scroll';
+    import SongList from 'components/SongList';
+    import Loading from 'components/Loading';
+    import { prefixStyle } from 'common/js/dom';
 
-const TOP_TITLE_HEIGHT = 40;
+    const TOP_TITLE_HEIGHT = 40;
 
-const prefixTransform = prefixStyle('transform');
-const prefixBackdrop = prefixStyle('backdrop-filter');
+    const prefixTransform = prefixStyle('transform');
+    const prefixBackdrop = prefixStyle('backdrop-filter');
 
-export default {
-    data() {
-        return {
-            scrollY: 0
-        };
-    },
-    mounted() {
-        const singerId = this.$route.params.id;
-
-        if (this.singerDetail.id !== singerId) {
-            this.resetSingerDetail();
-            this.fetchSingerDetail(singerId);
-        } else if (!singerId) {
-            this.$router.back();
-        }
-
-        this.bgHeight = window.innerWidth * 0.7;
-        this.$refs.listWrap.style.top = `${this.bgHeight}px`;
-        this.maxScrollY = this.bgHeight - TOP_TITLE_HEIGHT;
-    },
-    computed: {
-        bgStyle() {
-            return `background-image: url(${this.currentSinger.avatar || this.singerDetail.avatar})`;
+    export default {
+        data() {
+            return {
+                scrollY: 0
+            };
         },
-        ...mapGetters([
-            'singerDetail',
-            'currentSinger'
-        ])
-    },
-    methods: {
-        handleBack() {
-            this.$router.back();
-        },
-        handleScroll(pos) {
-            this.scrollY = pos.y;
-        },
-        handlePlaySong(item, index) {
-            this.setPlaySong({
-                list: this.singerDetail.list,
-                currentIndex: index
-            });
-        },
-        ...mapActions([
-            'fetchSingerDetail',
-            'resetSingerDetail',
-            'setPlaySong'
-        ])
-    },
-    watch: {
-        scrollY(newVal) {
-            const singerBgStyle = this.$refs.singerBg.style;
-            const playBtnStyle = this.$refs.playBtn.style;
-            const precent = Math.abs(newVal / this.bgHeight);
-            let zIndex = 0;
+        mounted() {
+            const singerId = this.$route.params.id;
 
-            if (newVal > 0) {
-                zIndex = 10;
-                const scaleChange = 1 + precent;
-                singerBgStyle[prefixTransform] = `scale(${scaleChange})`;
-            } else {
-                if (-newVal < this.maxScrollY) {
-                    const blur = 1 + Math.min(20, precent * 20);
-                    this.$refs.layerHook.style[prefixTransform] = `translateY(${newVal}px)`;
-                    singerBgStyle[prefixBackdrop] = `blur${blur}`;
-                    // 之所以改变背景图和标题同高是因为标题没有背景色无法遮住滚动的歌曲
-                    // 所以得通过改变背景图来达到效果
-                    singerBgStyle.paddingTop = `${this.bgHeight}px`;
-                    singerBgStyle.height = 0;
-                    playBtnStyle.display = '';
-                } else {
-                    singerBgStyle.paddingTop = 0;
-                    singerBgStyle.height = `${TOP_TITLE_HEIGHT}px`;
-                    playBtnStyle.display = 'none';
-                    zIndex = 10;
-                }
+            if (this.singerDetail.id !== singerId) {
+                this.resetSingerDetail();
+                this.fetchSingerDetail(singerId);
+            } else if (!singerId) {
+                this.$router.back();
             }
 
-            this.$refs.singerBg.style.zIndex = zIndex;
+            this.bgHeight = window.innerWidth * 0.7;
+            this.$refs.listWrap.style.top = `${this.bgHeight}px`;
+            this.maxScrollY = this.bgHeight - TOP_TITLE_HEIGHT;
+        },
+        computed: {
+            bgStyle() {
+                return `background-image: url(${this.currentSinger.avatar || this.singerDetail.avatar})`;
+            },
+            ...mapGetters([
+                'singerDetail',
+                'currentSinger'
+            ])
+        },
+        methods: {
+            handleBack() {
+                this.$router.back();
+            },
+            handleScroll(pos) {
+                this.scrollY = pos.y;
+            },
+            handlePlaySong(item, index) {
+                this.setPlaySong({
+                    list: this.singerDetail.list,
+                    currentIndex: index
+                });
+            },
+            ...mapActions([
+                'fetchSingerDetail',
+                'resetSingerDetail',
+                'setPlaySong'
+            ])
+        },
+        watch: {
+            scrollY(newVal) {
+                const singerBgStyle = this.$refs.singerBg.style;
+                const playBtnStyle = this.$refs.playBtn.style;
+                const precent = Math.abs(newVal / this.bgHeight);
+                let zIndex = 0;
+
+                if (newVal > 0) {
+                    zIndex = 10;
+                    const scaleChange = 1 + precent;
+                    singerBgStyle[prefixTransform] = `scale(${scaleChange})`;
+                } else {
+                    if (-newVal < this.maxScrollY) {
+                        const blur = 1 + Math.min(20, precent * 20);
+                        this.$refs.layerHook.style[prefixTransform] = `translateY(${newVal}px)`;
+                        singerBgStyle[prefixBackdrop] = `blur${blur}`;
+                        // 之所以改变背景图和标题同高是因为标题没有背景色无法遮住滚动的歌曲
+                        // 所以得通过改变背景图来达到效果
+                        singerBgStyle.paddingTop = `${this.bgHeight}px`;
+                        singerBgStyle.height = 0;
+                        playBtnStyle.display = '';
+                    } else {
+                        singerBgStyle.paddingTop = 0;
+                        singerBgStyle.height = `${TOP_TITLE_HEIGHT}px`;
+                        playBtnStyle.display = 'none';
+                        zIndex = 10;
+                    }
+                }
+
+                this.$refs.singerBg.style.zIndex = zIndex;
+            }
+        },
+        components: {
+            Scroll,
+            SongList,
+            Loading
         }
-    },
-    components: {
-        Scroll,
-        SongList,
-        Loading
-    }
-};
+    };
 </script>
 
 <style lang="scss" scoped>
-@import "~common/scss/variable";
-@import "~common/scss/mixin";
+    @import "~common/scss/variable";
+    @import "~common/scss/mixin";
 
-.slide-enter-active,
-.slide-leave-active {
-    transition: all .3s linear;
-}
-
-.slide-enter,
-.slide-leave-to {
-    transform: translateX(100%);
-}
-
-.singer-detail {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 100;
-    background-color: $color-background;
-
-    &-header {
-        display: flex;
-
-        .back {
-            position: absolute;
-            top: 0;
-            left: .6rem;
-            z-index: 50;
-
-            .icon-back {
-                display: block;
-                padding: 1rem;
-                font-size: $font-size-large-x;
-                color: $color-theme;
-            }
-        }
-
-        .title {
-            position: absolute;
-            top: 0;
-            left: 10%;
-            width: 80%;
-            @extend %no-wrap;
-            z-index: 50;
-            text-align: center;
-            line-height: 4rem;
-            font-size: $font-size-large;
-            color: $color-text;
-        }
+    .slide-enter-active,
+    .slide-leave-active {
+        transition: all .3s linear;
     }
 
-    &-image {
-        position: relative;
-        width: 100%;
-        height: 0;
-        padding-top: 70%;
-        background-size: cover;
-        transform-origin: top;
-
-        .filter {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(7, 17, 27, 0.4);
-        }
-
-        .play-btn {
-            position: absolute;
-            left: 50%;
-            transform: translateX(-50%);
-            bottom: 2rem;
-            padding: .7rem 2rem;
-            text-align: center;
-            border: .5px solid $color-theme;
-            color: $color-theme;
-            border-radius: 3rem;
-            font-size: 0;
-
-            .icon-play {
-                vertical-align: middle;
-                margin-right: .6rem;
-                font-size: $font-size-medium-x;
-            }
-
-            &-text {
-                vertical-align: middle;
-                font-size: $font-size-small;
-            }
-        }
+    .slide-enter,
+    .slide-leave-to {
+        transform: translateX(100%);
     }
 
-    &-layer {
-        position: relative;
-        height: 100%;
-        background-color: $color-background;
-    }
-
-    &-list {
+    .singer-detail {
         position: fixed;
         top: 0;
+        left: 0;
+        right: 0;
         bottom: 0;
-        width: 100%;
+        z-index: 100;
+        background-color: $color-background;
 
-        .m-scroll {
-            overflow: initial;
+        &-header {
+            display: flex;
+
+            .back {
+                position: absolute;
+                top: 0;
+                left: .6rem;
+                z-index: 50;
+
+                .icon-back {
+                    display: block;
+                    padding: 1rem;
+                    font-size: $font-size-large-x;
+                    color: $color-theme;
+                }
+            }
+
+            .title {
+                position: absolute;
+                top: 0;
+                left: 10%;
+                width: 80%;
+                @extend %no-wrap;
+                z-index: 50;
+                text-align: center;
+                line-height: 4rem;
+                font-size: $font-size-large;
+                color: $color-text;
+            }
         }
 
-        .song-list {
-            padding: 2rem 3rem;
+        &-image {
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-top: 70%;
+            background-size: cover;
+            transform-origin: top;
+
+            .filter {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color: rgba(7, 17, 27, 0.4);
+            }
+
+            .play-btn {
+                position: absolute;
+                left: 50%;
+                transform: translateX(-50%);
+                bottom: 2rem;
+                padding: .7rem 2rem;
+                text-align: center;
+                border: .5px solid $color-theme;
+                color: $color-theme;
+                border-radius: 3rem;
+                font-size: 0;
+
+                .icon-play {
+                    vertical-align: middle;
+                    margin-right: .6rem;
+                    font-size: $font-size-medium-x;
+                }
+
+                &-text {
+                    vertical-align: middle;
+                    font-size: $font-size-small;
+                }
+            }
+        }
+
+        &-layer {
+            position: relative;
+            height: 100%;
+            background-color: $color-background;
+        }
+
+        &-list {
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            width: 100%;
+
+            .m-scroll {
+                overflow: initial;
+            }
+
+            .song-list {
+                padding: 2rem 3rem;
+            }
         }
     }
-}
 </style>
